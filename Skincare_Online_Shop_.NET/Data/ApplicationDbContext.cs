ac54f78fd196fdc5efbafdae40ad8ce9619a87cd
andialexandrescu <147060135+andialexandrescu@users.noreply.github.com>
2024-11-18 16:59:28 +0200

    Add project files.

---
+++ Skincare_Online_Shop_.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skincare_Online_Shop_.NET.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
